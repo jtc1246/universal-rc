dd84b08fe3f57d794355931a372c4ba57d0c589e
jtc1246 <jtc1246@outlook.com>
2024-09-20 18:19:29 -0400

    fix

--- universal_rc.sh
+++ universal_rc.sh
@@ -1,5 +1,5 @@
 if ! (ps aux | grep -q "[u]niversal_rc_update.sh"); then
-    set +m; sh ~/.universal-rc/universal_rc_update.sh &
+    set +m; { sh ~/.universal-rc/universal_rc_update.sh & }; set -m
 fi
 
 function universal-rc() {
