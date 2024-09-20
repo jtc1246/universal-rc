128d617e3418b3eae4635082223e4201397c13d8
jtc1246 <jtc1246@outlook.com>
2024-09-20 16:53:49 -0400

    tmp

--- universal_rc.sh
+++ universal_rc.sh
@@ -1,4 +1,44 @@
 if ! (ps aux | grep -q "[u]niversal_rc_update.sh"); then
-    sh ~/.universal-rc/universal_rc_update.sh
+    sh ~/.universal-rc/universal_rc_update.sh &
     echo "update thread started"
 fi
+
+function universal-rc() {
+    if [ $# -eq 0 ]; then
+        echo "Universal-rc: Easily configure a global bashrc or zshrc file for all your devices."
+        echo -n "Last check: "
+        cat ~/.universal-rc/last_update.txt
+        echo -n "Current link: "
+        cat ~/.universal-rc/github_link.txt
+        echo 'Use "universal-rc update" to update the bashrc file.'
+        return 0
+    fi
+
+    if [ $# -eq 1 ]; then
+        if [ "$1" = "update" ]; then
+            sh ~/.universal-rc/universal_rc_update.sh 1
+            result=$?
+            if [ $result -eq 0 ]; then
+                echo "Update successfully."
+                return 0
+            else
+                echo "Failed to update the bashrc file, exit status: $result"
+                return $result
+            fi
+        fi
+        if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+            echo "Universal-rc: Easily configure a global bashrc or zshrc file for all your devices."
+            echo 'Use "universal-rc update" to update the bashrc file.'
+            return 0
+        fi
+        echo "Unrecognized command: $1"
+        echo 'Use "universal-rc -h" for help.'
+        return -1
+    fi
+
+    echo "Invalid number of arguments."
+    echo 'Use "universal-rc -h" for help.'
+    return -1
+}
+
+
--- universal_rc_update.sh
+++ universal_rc_update.sh
@@ -1,11 +1,17 @@
-# export GITHUB_LINK=""
+# export GITHUB_LINK="https://github.com/jtc1246/universal-rc"
+
+echo "$GITHUB_LINK/blob/main/bashrc.sh" > ~/.universal-rc/github_link.txt
 
 if [ $# -eq 0 ]; then
     # no argument, check updates forever
+    while true;
+    do
+        curl -s -f -L -o ~/.universal-rc/bashrc.sh "$GITHUB_LINK/blob/main/bashrc.sh?raw=True" && date > ~/.universal-rc/last_update.txt
+        sleep 5
+    done
 fi
 
 if [ $# -eq 1 ]; then
     # one argument, update once
-    rm -rf ~/.universal-rc/bashrc.sh
-    curl -o ~/.universal-rc/bashrc.sh "$GITHUB_LINK/bashrc.sh?raw=True"
+    curl -f -L -o ~/.universal-rc/bashrc.sh "$GITHUB_LINK/blob/main/bashrc.sh?raw=True" && date > ~/.universal-rc/last_update.txt
 fi
